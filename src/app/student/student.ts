5b145740be55e73e97b106b34b91fe22aac0da63
miner3001 <m.poputoaia.3594@vallauri.edu>
2026-09-21 10:19:31 +0200

    Utilizzato ngif per scrivere se lo studente è presente o assente

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -11,11 +11,11 @@ export class Student {
   MALE_COLOR ="lightblue";
   FEMALE_COLOR="pink";
   students: any[]=[
-    {name: "Pippo ",city:"topolinia",gender:"m",present:"true"},
-    {name: "Minnie ",city:"topolinia",gender:"f",present:"false"},
-    {name: "Rozzani ",city:"paperopoli",gender:"m",present:"true"},
-    {name: "Pippo Cattivo ",city:"paperopoli",gender:"f",present:"true"},
-    {name: "Peano",city:"topolinia",gender:"m",present:"true"}
+    {name: "Pippo ",city:"topolinia",gender:"m",present:true},
+    {name: "Minnie ",city:"topolinia",gender:"f",present:false},
+    {name: "Rozzani ",city:"paperopoli",gender:"m",present:true},
+    {name: "Pippo Cattivo ",city:"paperopoli",gender:"f",present:true},
+    {name: "Peano",city:"topolinia",gender:"m",present:true}
 
   ]
 }
